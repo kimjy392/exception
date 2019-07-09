aa31dd95266a1a3587bdd7181e490913c8fa129d
kimjunyoung <kimjy392@gmail.com>
2019-07-09 19:56:36 +0900

    대소문자 변환(%포맷팅 사용)

--- test.py
+++ test.py
@@ -5,3 +5,6 @@ if len(alph) == 1 and 64<asc and asc<91:
     print('%c(ASCII: %d) => %c(ASCII: %d)'%(asc, asc, asc+32, asc+32))
 elif len(alph) ==1 and 96<asc and asc<123:
     print('%c(ASCII: %d) => %c(ASCII: %d)'%(asc, asc, asc-32, asc-32))
+# .format()은 자료형을 지정하지 못한다.
+# % 포맷팅은 유니코드나 아스키값을 문자로 바꾸기 위해서는 %c을 써야한다.
+# upper()를 쓸수 있는건 비밀
